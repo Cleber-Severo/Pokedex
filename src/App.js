b8015a7370a5f225d19e9195bca9450ade80eadf
Cléber Severo <clebersevero1998@gmail.com>
2022-12-19 20:38:30 -0300

    grid for desktop screen

--- src/App.js
+++ src/App.js
@@ -10,10 +10,31 @@ function App() {
   const [pokemonData, setPokemonData] = useState([]);
   const [loading, setLoading] = useState(true);
 
+  const colours = {
+    normal: '#A8A77A',
+    fire: '#EE8130',
+    water: '#6390F0',
+    electric: '#F7D02C',
+    grass: '#7AC74C',
+    ice: '#96D9D6',
+    fighting: '#C22E28',
+    poison: '#A33EA1',
+    ground: '#E2BF65',
+    flying: '#A98FF3',
+    psychic: '#F95587',
+    bug: '#A6B91A',
+    rock: '#B6A136',
+    ghost: '#735797',
+    dragon: '#6F35FC',
+    dark: '#705746',
+    steel: '#B7B7CE',
+    fairy: '#D685AD',
+  };
+
   const getPokemons = async () => {
     try {
       var endpoints = [];
-      for (var i = 1; i < 200; i++) {
+      for (var i = 1; i < 500; i++) {
         endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/`);
       }
       await axios.all(endpoints.map((endpoint) => axios.get(endpoint))).then((res) => setPokemonData(res));
@@ -38,8 +59,7 @@ function App() {
       <section className='pokemon-list' >
         {pokemonData.map( pokemon => {
           const {name, order, sprites} = pokemon.data;
-          console.log(pokemon.data);
-          return <PokemonCard key={order} name={name} image={sprites.front_default} />
+          return <PokemonCard key={order} name={name} image={sprites.front_default}/>
         })}
       
       </section>
