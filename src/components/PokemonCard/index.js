26388f492572b25a878cc875e9f443ac7633ec3b
Cléber Severo <clebersevero1998@gmail.com>
2022-12-19 21:28:25 -0300

    card colors and type colors combining, better looking

--- src/components/PokemonCard/index.js
+++ src/components/PokemonCard/index.js
@@ -1,12 +1,16 @@
 
+import hexToRgba from 'hex-to-rgba';
+
+
 const PokemonCard = ({name, image, color, type}) => {
   
+  
   return (
-    <div className="pokemoncard" style={{ backgroundColor: color[type]}} >
+    <div className="pokemoncard" style={{ backgroundColor: hexToRgba(color[type], 0.8) }} >
         {console.log(type)}
         <img src={image} />
         <p>{name}</p>
-        <p>{type}</p>
+        <p style={{ backgroundColor: color[type]}} >{type}</p>
     </div>
   )
 }
