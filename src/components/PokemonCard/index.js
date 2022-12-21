d3c7f9dd3a5d68f6e034ce1a56125c89b8989199
Cléber Severo <clebersevero1998@gmail.com>
2022-12-20 23:10:11 -0300

    adding modal pokemon basic info as img and name

--- src/components/PokemonCard/index.js
+++ src/components/PokemonCard/index.js
@@ -63,10 +63,11 @@ const PokemonCard = ({name, image, color, type}) => {
       >
         <Box sx={style}>
           <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
+            {name}
+            <img src={image} alt="" style={{display: 'block'}} />
           </Typography>
           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            type:{typesHandler()}
           </Typography>
         </Box>
       </Modal>
