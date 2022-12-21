43835fddfd97a85bac5ea4e604c0c3df55761bf9
Cléber Severo <clebersevero1998@gmail.com>
2022-12-20 23:06:14 -0300

    adding a modal when clicking on card icon

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
     
     try {
       var endpoints = [];
-      for (var i = 1; i < 200; i++) {
+      for (var i = 1; i < 350; i++) {
         endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/`);
       }
       await axios.all(endpoints.map((endpoint) => axios.get(endpoint))).then((res) => setPokemonData(res));
--- src/components/PokemonCard/index.js
+++ src/components/PokemonCard/index.js
@@ -1,10 +1,31 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
 
 import hexToRgba from 'hex-to-rgba';
 import {AiFillInfoCircle} from 'react-icons/ai'
 
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 const PokemonCard = ({name, image, color, type}) => {
   
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   const pColor = {
     backgroundColor: color[type[0].type.name]
   };
@@ -25,13 +46,31 @@ const PokemonCard = ({name, image, color, type}) => {
   }
   
   return (
+
     <div className="pokemoncard" style={{ backgroundColor: hexToRgba(color[type[0].type.name], 0.6) }} >
-        <AiFillInfoCircle className='pokemoncard__icon' />
-        <img src={image} />
-        <div className='pokemoncard__container'>
-          <p className='pokemoncar__container--name'>{name}</p>
-          {typesHandler()}
-        </div>
+      <AiFillInfoCircle className='pokemoncard__icon' onClick={handleOpen}/>
+      <img src={image} />
+      <div className='pokemoncard__container'>
+        <p className='pokemoncar__container--name'>{name}</p>
+        {typesHandler()}
+      </div>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+
     </div>
   )
 }
