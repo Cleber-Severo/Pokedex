1bfa971f5c4037fc3ea00f3092997f2292ec6014
Cléber Severo <clebersevero1998@gmail.com>
2022-12-20 21:17:00 -0300

    pokecard dimensions fix

--- src/components/PokemonCard/index.js
+++ src/components/PokemonCard/index.js
@@ -14,21 +14,22 @@ const PokemonCard = ({name, image, color, type}) => {
     const p1Color = {backgroundColor: color[type[1].type.name]};
 
     return( 
-      <div>
+      <div className='type' >
         <p style={pColor}>{type[0].type.name}</p>
         <p style={p1Color}>{type[1].type.name}</p>
       </div>
       )
     }
-    return <p style={pColor} >{type[0].type.name}</p>
+    return <div className='type'><p style={pColor} >{type[0].type.name}</p></div>
   }
   
   return (
     <div className="pokemoncard" style={{ backgroundColor: hexToRgba(color[type[0].type.name], 0.8) }} >
         <img src={image} />
-        <p>{name}</p>
-        {/* <p style={{ backgroundColor: color[type]}} >{type}</p> */}
-        {typesHandler()}
+        <div className='pokemoncard__container'>
+          <p className='pokemoncar__container--name'>{name}</p>
+          {typesHandler()}
+        </div>
     </div>
   )
 }
