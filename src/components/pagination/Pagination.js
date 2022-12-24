eff258dee1a3eb371c0f26d719a1aedfe87d81bc
Cléber Severo <clebersevero1998@gmail.com>
2022-12-24 17:49:33 -0300

    pagination fix, now it shows only a few pages even when the app has hundreds of pages and pokemon that way it is better looking

--- src/components/pagination/Pagination.js
+++ src/components/pagination/Pagination.js
@@ -7,19 +7,85 @@ const Pagination = ({pagesNum, cardsPerPage, changePage, currentPage}) => {
         pages.push(i);
     }
 
+    
+    if(currentPage === 1) {
+        
+        return (
+            <div className='pagination' >
+                {pages.map((page, index) =>{  
+
+                    if(page == currentPage){
+                        return(
+                        <div>
+                            
+                            <button key={index} onClick={() => { changePage(page); }} className={ page == currentPage ? 'active' : ''}>{page}</button>
+                            <button key={index} onClick={() => { changePage(page +1 ); }} className={ page +1  == currentPage ? 'active' : ''}>{page+ 1}</button>
+                            <button key={index} onClick={() => { changePage(page +2 ); }} className={ page +2 == currentPage ? 'active' : ''}>{page + 2}</button>
+                            <button key={index} onClick={() => { changePage(page +1); }} > {`>`} </button>
+
+
+
+                            {/* <button 
+                                key={index} 
+                                onClick={() => { changePage(page); }}
+                                className={ page == currentPage ? 'active' : ''}
+                            >
+                                {page}
+                            </button> */}
+                        </div>
+                        
+                    )
+                    }
+                    
+                })}
+            </div>
+        )
+
+    }
+
+
     return (
         <div className='pagination'>
             {/* {console.log(pages)} */}
-            {pages.map((page, index) =>{
-                return (
-                    <button 
-                        key={index} 
-                        onClick={() => { changePage(page); }}
-                        className={ page == currentPage ? 'active' : ''}
-                    >
-                        {page}
-                    </button>
-                )
+            {pages.map((page, index) =>{            
+        
+                 if(page == currentPage){
+                    return(
+                        <div>
+                            <button key={index} onClick={() => { changePage(page -1); }} > {`<`} </button>
+                            <button key={index} onClick={() => { changePage(page -2 ); }} className={ page +1  == currentPage ? 'active' : ''} style={page == 2 ? {display:'none'}:{}} >{page == 2 ? '' : page - 2}</button>
+                            <button key={index} onClick={() => { changePage(page -1 ); }} className={ page +2 == currentPage ? 'active' : ''}>{page - 1}</button>
+                           
+
+                            <button key={index} onClick={() => { changePage(page); }} className={ page == currentPage ? 'active' : ''}>{page}</button>
+                            <button key={index} onClick={() => { changePage(page +1 ); }} className={ page +1  == currentPage ? 'active' : ''}>{page+ 1}</button>
+                            <button key={index} onClick={() => { changePage(page +2 ); }} className={ page +2 == currentPage ? 'active' : ''}>{page + 2}</button>
+                            <button key={index} onClick={() => { changePage(page +1); }} > {`>`} </button>
+
+
+
+                            {/* <button 
+                                key={index} 
+                                onClick={() => { changePage(page); }}
+                                className={ page == currentPage ? 'active' : ''}
+                            >
+                                {page}
+                            </button> */}
+                        </div>
+                        
+                    )
+                }
+
+        //         return (
+        //             <button 
+        //                 key={index} 
+        //                 onClick={() => { changePage(page); }}
+        //                 className={ page == currentPage ? 'active' : ''}
+        //             >
+        //                 {page}
+        //             </button>
+        //         )
+
             })}
         </div>
   )
